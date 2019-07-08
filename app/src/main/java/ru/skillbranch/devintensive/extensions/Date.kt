2b551b9514592b1dcd074f75180cf8e3f14aae31
ThomazMerzer <thomazmerzer@gmail.com>
2019-07-08 16:27:29 +0500

    Закончен plurals

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -98,5 +98,26 @@ enum class TimeUnits {
     SECOND,
     MINUTE,
     HOUR,
-    DAY
+    DAY;
+}
+
+fun TimeUnits.plurals(value: Int): String {
+    var tempStr = ""
+
+    var one = arrayOf(1, 21, 31, 41, 51)
+    val twoToFour = arrayOf(2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54)
+    val five = arrayOf(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25, 26, 27,
+        28, 29, 30, 35, 36, 37, 38, 39, 40, 45, 46, 47, 48, 49, 50, 55, 56, 57, 58, 59, 60)
+
+    val timeUnit = this.name
+
+    tempStr = when (timeUnit) {
+        "SECOND" -> if (value in one) "секунду" else if (value in twoToFour) "секунды" else "секунд"
+        "MINUTE" -> if (value in one) "минуту" else if (value in twoToFour) "минуты" else "минут"
+        "HOUR" -> if (value in one) "ас" else if (value in twoToFour) "часа" else "часов"
+        "DAY" -> if (value in one) "день" else if (value in twoToFour) "дня" else "дней"
+        else -> ""
+    }
+
+    return "$value $tempStr"
 }
