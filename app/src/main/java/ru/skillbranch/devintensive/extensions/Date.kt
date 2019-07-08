080121092b0b16d1357e5b9492d58a667501c9a8
ThomazMerzer <thomazmerzer@gmail.com>
2019-07-08 09:55:21 +0500

    Закончил humanizeDiff функцию. Склонение пока статичное

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -28,7 +28,70 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    TODO("not implemented") //Написать функцию для показа времени, когда пользователь последний раз был в сети
+
+    val customDate = (this.time / 1000).toInt()
+    val currentDate = (Date().time / 1000).toInt()
+    //Перевёрнем число в положительную сторону для облегчения чтения
+    var diff = (customDate - currentDate) - ((customDate - currentDate) * 2)
+
+    val backWord = "назад"
+    var tempString = ""
+    var timeUnit = ""
+
+    when {
+        diff in -1..1 ->
+            //Был только что
+            tempString = "только что"
+        diff in 1..44 -> {
+            //Был несколько секунд назад
+            timeUnit = "секунд"
+            tempString = "несколько $timeUnit $backWord"
+        }
+        diff in 44..75 -> {
+            //Был минуту назад
+            timeUnit = "минуту"
+            tempString = "$timeUnit $backWord"
+        }
+        diff in 75..2700 -> {
+            //Был несколько минут назад
+            timeUnit = "минут"
+            diff /= 60
+            tempString = "$diff $timeUnit $backWord"
+        }
+        diff in 2700..4500 -> {
+            //Был час назад
+            timeUnit = "час"
+            tempString = "$timeUnit $backWord"
+        }
+        diff in 4500..79200 -> {
+            //Был несколько часов назад
+            timeUnit = "часов"
+            diff = diff / 60 / 60
+            tempString = "$diff $timeUnit $backWord"
+        }
+        diff in 79200..93600 -> {
+            //Был день назад
+            timeUnit = "день"
+            tempString = "$timeUnit $backWord"
+        }
+        diff in 93600..31104000 -> {
+            //Был несколько дней назад
+            timeUnit = "дней"
+            diff = diff / 60 / 60 / 24
+            tempString = "$diff $timeUnit $backWord"
+        }
+        diff > 31104000 -> {
+            //Был более 360 дней
+            timeUnit = "года"
+            tempString = "более $timeUnit $backWord"
+        }
+        else -> {
+            tempString = "неизвестно когда"
+        }
+    }
+
+    println("Пользователь был в сети $tempString")
+    return tempString
 }
 
 enum class TimeUnits {
