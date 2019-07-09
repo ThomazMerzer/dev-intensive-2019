597296c0926a829bb0e0d002abb18a917cd42d78
ThomazMerzer <thomazmerzer@gmail.com>
2019-07-09 20:55:19 +0500

    truncate переписан под kotlin

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,31 +1,48 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(value: Int = 16): String {
-    var index = 0
+//    var index = 0
+//
+//    if(value > this.length) throw StringIndexOutOfBoundsException("Число больше длины строки value: $value")
+//
+//    return if(this.last() == ' ' && value != 0) {
+//        for (char in this) {
+//            if (char != ' ') {
+//                index++
+//            }
+//        }
+//        println("${this.substring(0, index)}...")
+//        "${this.substring(0, index)}..."
+//    }
+//    else if(value != 0){
+//        println("${this.substring(0, value)}...")
+//        "${this.substring(0, value)}..."
+//    }
+//    else if(value == 0){
+//        index = this.length
+//        println("${this.substring(0, index)}...")
+//        "${this.substring(0, index)}..."
+//    }
+//    else {
+//        println(this)
+//        this
+//    }
 
-    if(value > this.length) throw StringIndexOutOfBoundsException("Число больше длины строки value: $value")
+    val strSize = this.trim().length
+    var str = this.trim().take(value)
 
-    return if(this.last() == ' ' && value != 0) {
-        for (char in this) {
-            if (char != ' ') {
-                index++
+    return when {
+        strSize != str.length -> {
+            if(str.toCharArray().last().isWhitespace()) {
+                str = str.dropLast(1)
             }
+            println("$str...")
+            "$str..."
         }
-        println("${this.substring(0, index)}...")
-        "${this.substring(0, index)}..."
-    }
-    else if(value != 0){
-        println("${this.substring(0, value)}...")
-        "${this.substring(0, value)}..."
-    }
-    else if(value == 0){
-        index = this.length
-        println("${this.substring(0, index)}...")
-        "${this.substring(0, index)}..."
-    }
-    else {
-        println(this)
-        this
+        else ->  {
+            println(str)
+            str
+        }
     }
 }
 
